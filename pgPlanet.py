b7e689125e2ea796769bd043b9c29ef2313feb40
ray_ank <rayank@blub.de>
2018-06-02 09:45:25 +0200

    simple rect flying

--- pgPlanet.py
+++ pgPlanet.py
@@ -14,6 +14,13 @@ def main():
 
     clock = pygame.time.Clock()
     running = True
+    x=600
+    y=400
+    direction_x=1
+    direction_y=1
+    speed_x=5
+    speed_y=5
+
     while running:
         clock.tick(30) # frame rate 30 ticks
         screen.fill((0, 0, 0))
@@ -21,11 +28,24 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
-                print "keydown"
                 if event.key == pygame.K_ESCAPE:
-                    print "escape"
                     pygame.event.post(pygame.event.Event(pygame.QUIT))
-            pygame.display.flip()
+        pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(x,y, 20, 20))
+        if(direction_x==1):
+            if(x>770):
+                direction_x=-1
+        if(direction_x==-1):
+            if(x<0):
+                direction_x=1
+        if(direction_y==1):
+            if(y>570):
+                direction_y=-1
+        if(direction_y==-1):
+            if(y<0):
+                direction_y=1
+        x=x+direction_x*speed_x
+        y=y+direction_y*speed_y
+        pygame.display.flip()
 
 if __name__ == '__main__':
     main()

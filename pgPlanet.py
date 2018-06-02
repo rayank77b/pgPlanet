fca0f6192f827f2e3e5de441841de9353d3fe415
ray_ank <rayank@blub.de>
2018-06-02 14:29:25 +0200

    hm, something is wrong with acceleration

--- pgPlanet.py
+++ pgPlanet.py
@@ -5,6 +5,14 @@ import math
 
 X_SCREEN=1024
 Y_SCREEN=800
+RASTER=1000
+ACCEL=50.0
+
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE =  (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED =   (255,   0,   0)
 
 class Mass:
     def __init__(self,x,y,d,xv,yv, xa, ya):
@@ -17,14 +25,6 @@ class Mass:
         self.accel_y=ya
     
     def calc_xy(self):
-        if(self.x>(X_SCREEN-self.d)):
-            self.speed_x=-1*self.speed_x
-        if(self.x<0):
-            self.speed_x=-1*self.speed_x
-        if(self.y>(Y_SCREEN-self.d)):
-            self.speed_y=-1*self.speed_y
-        if(self.y<0):
-            self.speed_y=-1*self.speed_y
         self.x = self.x + self.speed_x
         self.y = self.y + self.speed_y
     
@@ -37,7 +37,7 @@ class Mass:
         for m in masses:
             if((self.x!=m.x) and (self.y!=m.y)):
                 delta = self._triangle(m.x, m.y)
-                c = (self.d*m.d)/(delta*delta)
+                c = ACCEL*(self.d*m.d)/(delta*delta)
                 if(self.x<m.x):
                     self.accel_x = self.accel_x + c
                 else:
@@ -50,7 +50,13 @@ class Mass:
         self.speed_y = self.speed_y + self.accel_y
     
     def draw_me(self, screen):
-        pygame.draw.rect(screen, (0, 128+self.d, 255), pygame.Rect(self.x, self.y, self.d, self.d))
+        # draw the object
+        pygame.draw.circle(screen, BLUE, [int(self.x/RASTER), int(self.y/RASTER)], self.d)
+        # draw the veloecity
+        pygame.draw.line(screen, WHITE, [int(self.x/RASTER), int(self.y/RASTER)], [int(self.x/RASTER)+self.speed_x, int(self.y/RASTER)+self.speed_y], 1)
+        # draw the acceleration
+        pygame.draw.line(screen, RED, [int(self.x/RASTER), int(self.y/RASTER)], [int(self.x/RASTER)+self.accel_x*1000, int(self.y/RASTER)+self.accel_y*1000], 1)
+
 
 def key_events():
     for event in pygame.event.get():
@@ -78,10 +84,16 @@ def main():
     running=True
     
     m=list()
-    for i in range(2):
-        m.append(Mass(randint(0,X_SCREEN),randint(0,Y_SCREEN),randint(2,10), randint(-5,5),randint(-5,5),0,0))
+    for i in range(10):
+        x=randint(350*X_SCREEN, X_SCREEN*750)
+        y=randint(350*Y_SCREEN, Y_SCREEN*750)
+        radius = randint(2,5)
+        xv = randint(-15,15)
+        yv = randint(-15,15)
+        print "( %d | %d ) r: %d   xv: %d  yv: %d"%(int(x/RASTER),int(y/RASTER), radius, xv, yv)
+        m.append(Mass(x,y,radius, xv, yv,0,0))
     while running:
-        clock.tick(60) # frame rate 60 ticks
+        #clock.tick(60) # frame rate 60 ticks
         screen.fill((0, 0, 0))
         running = key_events()
         for m1 in m:
